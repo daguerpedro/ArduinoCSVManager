d5e41388458cf80c90019549409be8f5cbeeac3d
Pedro Daguer <158527683+daguerpedro@users.noreply.github.com>
2024-04-15 14:17:08 -0300

    refatorado

--- novo.py
+++ novo.py
@@ -1,51 +1,96 @@
-import csv
-from prettytable import PrettyTable
-import os
+import csv, os, random
+from time import sleep
+import time
 
-csvFile = 'tabela.csv'
-variaveis = ["A", "B"]
+variaveis = ["H", "V1", "A1", "P1", "V2", "A2", "P2"]
+digits = 18 #Quantos digitos vamos alinhar
 
-tabelaconsole = PrettyTable()
-tabelaconsole.field_names = variaveis
+def printtable(data):
+    last = (len(data) - 1)
+
+    print('|', end = '')
+    for d in data:
+        pos = data.index(d)     
+
+        divisor = ','
+        
+        stringsize = len(str(d)) #Tamanho do dado recebido
+        strtoprint = str(d) #Dodo formatado como string
+        strformatado = strtoprint
+
+        if stringsize < digits: #Precisa alinhar
+            missing = digits - stringsize #Quantos faltam para alinhar com 7digitos
+
+            prefix = sufix = ''
+
+            for i in range(0, missing // 2):
+                prefix += ' '
+                sufix += ' '
+
+            if not (missing % 2) == 0:
+                sufix += ' '
+
+            strformatado = prefix + strtoprint + sufix
+             
+
+        if pos >= last:
+            divisor = '|\n'
+
+        print(strformatado, end = divisor, flush=True)
+
+def arquivoCSV() -> str:
+    c = f'tabela{time.strftime("_%d_%m_%Y", time.localtime(time.time()))}.csv'
+    if not os.path.exists(c):
+        try:
+            with open(c, 'a', newline='\n') as file: # Iremos adicionar algo na última linha, e não substiur o arquivo inteiro.
+                tabelacsv = csv.writer(file)
+                tabelacsv.writerow(variaveis)
+                file.close()          
+        except (OSError, Exception) as e:
+            print(f"[ERRO]\nOcorreu um erro enquanto tentava salvar o cabeçalho na tabela nova: {c}!\n{e}\n")
+            pass
+    return c
 
 def saveToCSV(dados):
     try:
-        existe = os.path.exists(csvFile)
-        with open(csvFile, 'a', newline='') as file: # Iremos adicionar algo na última linha, e não substiur o arquivo inteiro.
+        with open(arquivoCSV(), 'a', newline='\n') as file: # Iremos adicionar algo na última linha, e não substiur o arquivo inteiro.
             tabelacsv = csv.writer(file)
-            if not existe: # Se não existe, vamos criar os parametros, caso contrário vamos apenas adicionar linhas
-                tabelacsv.writerow(variaveis)
-            
             if not file.closed:
                 tabelacsv.writerow(dados)
-                tabelaconsole.add_row(dados)
-                print(tabelaconsole)
-            
                 file.close()
             else:
-                print(f"!!!! [ERRO] !!!!\nO arquivo {csvFile} estava fechado enquanto tentei salvar dados!\n")
+                print(f"[AVISO]\nO arquivo {csvFile} estava fechado enquanto tentei salvar dados!\n")
                 
-    except OSError as e:
-        print(f"!!!! [ERRO] !!!!\nOcorreu um erro enquanto tentava salvar a tabela!\n{e.winerror}\n")
-        print(e)
+    except (OSError, Exception) as e:
+        print(f"[ERRO]\nOcorreu um erro enquanto tentava salvar a tabela!\n{e}\n")
         pass
 
 def load():
-    existe = os.path.exists(csvFile)
-    if not existe:
-        return
+    try:
+        with open(arquivoCSV(), 'r', newline='\n') as file:
+            tabelacsv = csv.reader(file)
+
+            for linha in tabelacsv:
+                printtable(linha)
+            
+            file.close()
+    except (OSError, Exception) as e:
+        print(f"[ERRO]\nOcorreu um erro enquanto tentava carregar a tabela que já existia!\n{e}\n")
+        pass
     
-    with open(csvFile, 'r', newline='') as file:
-        tabelacsv = csv.reader(file)
-        cabecalho = next(tabelacsv)
-        tabelaconsole.field_names = cabecalho
-        for linha in tabelacsv:
-            tabelaconsole.add_row(linha)
-        print(tabelaconsole)
+def read():
+    for i in range(0, 15): # ADICIONAR COMUNICACAO SERIAL:
+        data = [time.strftime("%H:%M:%S;%d/%m", time.localtime(time.time())), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)]
+        saveToCSV(data)
+        printtable(data)
+        sleep(.5)
 
-        file.close()
+def init():
+    try:
+        load()
+        read()
+    except KeyboardInterrupt:
+        print("Programa finalizado.")
 
-load()
-
-for i in range(0, 10):
-    saveToCSV([i, pow(i, 2)])
+if __name__ == "__main__":
+    init()
