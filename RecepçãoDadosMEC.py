af1662154d600648e48493c474eeb25497de3c7a
Daguer <158527683+daguerpedro@users.noreply.github.com>
2024-08-13 12:54:24 -0300

    api usage

--- RecepçãoDadosMEC.py
+++ RecepçãoDadosMEC.py
@@ -3,15 +3,15 @@ from utils.coms import *
 from utils.pytable import *
 from utils.API import *
 
-table = PyTable()
-
-variaveis = ["DIA", "HORA", "mV (MEC1)", "mA (MEC1)", "mW (MEC1)", "mV (MEC2)", "mA (MEC2)", "mW (MEC2)", "°C"]
-
-table.alignmentDigits = 12 # Quantos digitos vamos alinhar a tabela no console
-
 comport = "COM4" # Altere para a porta desejada
 baud = 9600 # Frequência desejada
 
+variaveis = ["DIA", "HORA", "mV (MEC1)", "mA (MEC1)", "mW (MEC1)", "mV (MEC2)", "mA (MEC2)", "mW (MEC2)", "°C"]
+
+table = PyTable()
+table.alignmentDigits = 12 # Quantos digitos vamos alinhar a tabela no console
+
+useAPI = False
 api = API(apiURL = 'http://localhost:8080/data/add')
 
 fileManager = FileSaver(variaveis=variaveis)
@@ -24,20 +24,21 @@ def aoReceber(dados):
     recebidos = dados
     recebidos.pop(0) # remover a data
 
-    try:
-        res = api.post({
-        "hora": str(dados[0]), 
-        "tensao1": str(dados[1]), 
-        "corrente1": str(dados[2]), 
-        "potencia1": str(dados[3]), 
-        "tensao2": str(dados[4]), 
-        "corrente2": str(dados[5]), 
-        "potencia2": str(dados[6]), 
-        "temperatura": str(dados[7])
-        })
+    if useAPI == True:
+        try:
+            res = api.post({
+            "hora": str(dados[0]), 
+            "tensao1": str(dados[1]), 
+            "corrente1": str(dados[2]), 
+            "potencia1": str(dados[3]), 
+            "tensao2": str(dados[4]), 
+            "corrente2": str(dados[5]), 
+            "potencia2": str(dados[6]), 
+            "temperatura": str(dados[7])
+            })
 
-    except:
-        pass
+        except:
+            pass
 
 def processarLinhasSalvas(linha):
     table.addRow(linha)
